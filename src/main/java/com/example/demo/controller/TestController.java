5b0a8ef92b94e924e43b4a93a08c43e4ddabcc42
hskim <simplebest2k@naver.com>
2021-10-26 21:08:08 +0900

    testcontroller 추가 test1 함수 추가

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+   @GetMapping("/test1")
+   public String test1() {
+      return "test1";
+   }
+}
